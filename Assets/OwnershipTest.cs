f0fe7a7efc04bbd986860c044a33bb23bca7d3bb
Martin <e11909911@student.tuwien.ac.at>
2022-12-04 18:32:30 +0100

    Make colors more pronounced

--- Assets/OwnershipTest.cs
+++ Assets/OwnershipTest.cs
@@ -5,7 +5,14 @@ using UnityEngine;
 public class OwnershipTest : MonoBehaviourPun {
     private static Dictionary<int, Color> Colors = new();
 
-    void Update() {
+    private void Start() {
+        Colors.Add(0, Color.red);
+        Colors.Add(1, Color.blue);
+        Colors.Add(2, Color.green);
+        Colors.Add(3, Color.yellow);
+    }
+
+    private void Update() {
         this.GetComponent<Renderer>().material.color = Colors.GetOrAddWith(photonView.OwnerActorNr, k => {
             var random = new System.Random(k);
             return new Color(random.NextFloat(), random.NextFloat(), random.NextFloat());
