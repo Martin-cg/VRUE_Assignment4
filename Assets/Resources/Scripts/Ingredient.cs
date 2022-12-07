d0ad5deb52c9c3ab211676a1fffedc8aa40d64fa
AdrianGawor <e11905152@student.tuwien.ac.at>
2022-12-07 18:00:50 +0100

    Introduce Item States into Test Item.

--- Assets/Resources/Scripts/Ingredient.cs
+++ Assets/Resources/Scripts/Ingredient.cs
@@ -3,4 +3,45 @@ using UnityEngine.XR.Interaction.Toolkit;
 
 [RequireComponent(typeof(XRGrabInteractable))]
 public class Ingredient : MonoBehaviour {
+
+    public enum IngredientState {
+        Initial,
+        Chopped,
+        Cooked
+    }
+
+    public IngredientState CurrentState = IngredientState.Initial;
+
+    public GameObject InitialStateModel;
+    public GameObject ChoppedStateModel;
+    public GameObject CookedStateModel;
+
+    private void Update() {
+        HideAllStates();
+        ShowActiveState();
+    }
+
+    private void HideAllStates() {
+        InitialStateModel.SetActive(false);
+        ChoppedStateModel.SetActive(false);
+        CookedStateModel.SetActive(false);
+    }
+
+    private void ShowActiveState() {
+        switch (CurrentState) {
+            case IngredientState.Initial:
+                InitialStateModel.SetActive(true);
+                break;
+            case IngredientState.Chopped:
+                ChoppedStateModel.SetActive(true);
+                break;
+            case IngredientState.Cooked:
+                CookedStateModel.SetActive(true);
+                break;
+            default:
+                InitialStateModel.SetActive(true);
+                break;
+        }
+    }
+
 }
