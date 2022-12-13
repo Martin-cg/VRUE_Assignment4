3878f4271b8731f81596a60b9104ad6e4b15043c
Martin <e11909911@student.tuwien.ac.at>
2022-12-13 20:24:56 +0100

    Fix initial value for room properties and check for duplicate keys

--- Assets/Resources/Scripts/SynchronizedRoomObject.cs
+++ Assets/Resources/Scripts/SynchronizedRoomObject.cs
@@ -5,26 +5,57 @@ using UnityEngine;
 using Hashtable = ExitGames.Client.Photon.Hashtable;
 
 public class SynchronizedRoomObject : MonoBehaviourPunCallbacks {
+    public bool ShareAcrossBehavioursOnSameObject = false;
+
+    private const string KeySeparator = "/";
+    private static readonly HashSet<string> RegisteredKeys = new();
     private readonly List<ISynchronizedRoomProperty> Properties = new();
     private readonly Hashtable CurrentPropertyTableCache = new();
     private readonly Hashtable PreviousPropertyTableCache = new();
     private string ScenePath;
+    private string BehaviourName;
+    private bool Started = false;
 
     protected virtual void Awake() {
         InOnlineRoomOnly.AddTo(this);
     }
 
     protected virtual void Start() {
-        ScenePath = gameObject.GetScenePathString();
+        ScenePath = gameObject.GetScenePathString(null, true, true, KeySeparator);
+        BehaviourName = GetType().FullName;
+
+        Started = true;
+        foreach (var property in Properties) {
+            HandleInitialPropertyValue(property);
+        }
     }
 
     protected SynchronizedRoomProperty<T> RegisterProperty<T>(string name, T startingValue, string key=null) {
-        var property = new SynchronizedRoomProperty<T>(name, key ?? GetKeyForName(name), startingValue, this);
+        key ??= GetKeyForName(name);
+        if (Application.isEditor) {
+            var added = RegisteredKeys.Add(key);
+            if (!added) {
+                Debug.LogError($"[RoomProperties] Detected duplicate room property key '{key}'");
+            }
+        }
+        var property = new SynchronizedRoomProperty<T>(name, key, startingValue, this);
         Properties.Add(property);
-        if (PhotonNetwork.CurrentRoom.CustomProperties.TryGetValue(property.Key, out var currentValue)) {
-            SetRoomProperty(property.Key, currentValue);
+        if (Started) {
+            HandleInitialPropertyValue(property);
         }
         return property;
+    }
+
+    private void HandleInitialPropertyValue(ISynchronizedRoomProperty property) {
+        Debug.Assert(Started, this);
+        Debug.Assert(PhotonNetwork.CurrentRoom != null, this);
+
+        if (PhotonNetwork.CurrentRoom.CustomProperties.TryGetValue(property.Key, out var currentValue)) {
+            property.Value = currentValue;
+        } else {
+            // Should not be required as every client should start with the same inital value.
+            // SetRoomProperty(property.Key, currentValue);
+        }
     }
 
     private void SetRoomProperty(string key, object value) {
@@ -38,7 +69,11 @@ public class SynchronizedRoomObject : MonoBehaviourPunCallbacks {
     }
 
     private string GetKeyForName(string name) {
-        return $"{ScenePath}/{name}";
+        var key = ScenePath + KeySeparator + name;
+        if (!ShareAcrossBehavioursOnSameObject) {
+            key += KeySeparator + BehaviourName;
+        }
+        return key;
     }
 
     public override void OnRoomPropertiesUpdate(Hashtable propertiesThatChanged) {
@@ -64,19 +99,20 @@ public class SynchronizedRoomObject : MonoBehaviourPunCallbacks {
         public event EventHandler<T> ValueChanged;
         public SynchronizedRoomObject Object { get; private set; }
         public IEqualityComparer<T> Comparer { get; private set; }
-        public T Value { get; private set; }
+        public T _Value;
+        public T Value { get => _Value; set => SetValue(value); }
         object ISynchronizedRoomProperty.Value { get => Value; set => Value = (T) value; }
 
         public void SetValue(T newValue, bool notifyLocal=false, bool notifyRemote=true) {
-            if (Comparer.Equals(Value, newValue)) {
+            if (Comparer.Equals(_Value, newValue)) {
                 return;
             }
-            Value = newValue;
+            _Value = newValue;
             if (notifyLocal) {
-                ValueChanged?.Invoke(this, Value);
+                ValueChanged?.Invoke(this, _Value);
             }
             if (notifyRemote) {
-                Object.SetRoomProperty(Key, Value);
+                Object.SetRoomProperty(Key, _Value);
             }
         }
         void ISynchronizedRoomProperty.SetValue(object newValue, bool notifyLocal, bool notifyRemote) => SetValue((T)newValue, notifyLocal, notifyRemote);
@@ -84,7 +120,7 @@ public class SynchronizedRoomObject : MonoBehaviourPunCallbacks {
         internal SynchronizedRoomProperty(string name, string key, T startingValue, SynchronizedRoomObject @object, IEqualityComparer<T> comparer=null) {
             Name = name;
             Key = key;
-            Value = startingValue;
+            _Value = startingValue;
             Object = @object;
             Comparer = comparer ?? StructuralEqualityComparer<T>.Default;
         }
