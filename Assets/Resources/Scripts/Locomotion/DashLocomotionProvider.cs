10737e81f63b76ad12fc14f658e47117f4e51144
Martin <e11909911@student.tuwien.ac.at>
2022-12-15 19:57:23 +0100

    Finish Dash locomotion

--- Assets/Resources/Scripts/Locomotion/DashLocomotionProvider.cs
+++ Assets/Resources/Scripts/Locomotion/DashLocomotionProvider.cs
@@ -1,4 +1,3 @@
-using System.Linq;
 using UnityEngine;
 using UnityEngine.XR.Interaction.Toolkit;
 
@@ -6,12 +5,16 @@ using UnityEngine.XR.Interaction.Toolkit;
 public class DashLocomotionProvider : ContinuousMoveProviderBase {
     public GameObject LeftHand;
     public GameObject RightHand;
+    public float Duration = 0.33f;
+    public float CurveExponent = 3;
 
     private const int PreviousPositionCount = 2;
     private Vector3[] PreviousLeftPositions = new Vector3[PreviousPositionCount];
     private Vector3[] PreviousRightPositions = new Vector3[PreviousPositionCount];
 
-    private float Velocity = 0.0F;
+    public bool IsDashing { get; private set; }
+    private float DashTime;
+    private float DashModifier;
 
     // Start is called before the first frame update
     void Start() {
@@ -29,6 +32,18 @@ public class DashLocomotionProvider : ContinuousMoveProviderBase {
         }
     }
 
+    protected new void Update() {
+        if (IsDashing) {
+            DashTime += Time.deltaTime * (1/DashModifier);
+            if (DashTime > Duration) {
+                IsDashing = false;
+                DashTime = 0;
+            }
+        }
+
+        base.Update();
+    }
+
     protected override Vector2 ReadInput() {
         for (int i = 1; i < PreviousPositionCount; i++) {
             PreviousLeftPositions[i] = PreviousLeftPositions[i - 1];
@@ -37,18 +52,24 @@ public class DashLocomotionProvider : ContinuousMoveProviderBase {
         PreviousLeftPositions[0] = LeftHand.transform.position;
         PreviousRightPositions[0] = RightHand.transform.position;
 
+        if (IsDashing) {
+            return GetCurrentDashVelocity();
+        }
+
         var leftVelocity = PreviousLeftPositions[0] - PreviousLeftPositions[PreviousPositionCount - 1];
-        var rightVelocity = PreviousRightPositions[0] - PreviousRightPositions[PreviousPositionCount-1];
+        var rightVelocity = PreviousRightPositions[0] - PreviousRightPositions[PreviousPositionCount - 1];
 
         var leftMagnitude = leftVelocity.magnitude;
         var rightMagnitude = rightVelocity.magnitude;
 
         // Ignore small movements
-        if (Mathf.Max(leftMagnitude, rightMagnitude) < 0.001 * PreviousPositionCount) {
+        if (Mathf.Max(leftMagnitude, rightMagnitude) < 0.01 * PreviousPositionCount) {
             return Vector2.zero;
         }
 
         var forward = forwardSource.transform.forward;
+        forward = Vector3.ProjectOnPlane(forward, transform.up);
+
         var dotProductLeft = Vector3.Dot(forward, leftVelocity.normalized);
         var dotProductRight = Vector3.Dot(forward, rightVelocity.normalized);
         var combined = dotProductLeft * dotProductRight;
@@ -58,18 +79,52 @@ public class DashLocomotionProvider : ContinuousMoveProviderBase {
             return Vector2.zero;
         }
 
-        // Ignore movement of only one hand
-        // if (Mathf.Max(leftMagnitude, rightMagnitude)*2 > Mathf.Min(leftMagnitude, rightMagnitude)) {
-        //     return Vector2.zero;
-        // }
+        var leftDirectedMagnitude = -Vector3.Dot(forward, leftVelocity);
+        var rightDirectedMagnitude = -Vector3.Dot(forward, rightVelocity);
 
-        var swingStrength = combined * (leftMagnitude + rightMagnitude);
-        if (swingStrength >= 0.1 * PreviousPositionCount) {
-            Velocity = 1;
-            Debug.LogWarning("DASH!");
-            ResetPositions();
+        // Ignore movement of only one hand
+        Debug.Log($"{(Mathf.Min(leftDirectedMagnitude, rightDirectedMagnitude) * 2):0.00000} {(Mathf.Max(leftDirectedMagnitude, rightDirectedMagnitude)):0.00000}");
+        if (Mathf.Min(leftDirectedMagnitude, rightDirectedMagnitude)*2 < Mathf.Max(leftDirectedMagnitude, rightDirectedMagnitude)) {
+            return Vector2.zero;
         }
 
-        return Velocity <= 0 ? Vector2.zero : new Vector2(0, Velocity); // This weirdness is left as an exercise for the reader
+        // Ignore slow movement
+        var combinedDirectedMagnitude = leftDirectedMagnitude * rightDirectedMagnitude;
+        Debug.Log($"{combinedDirectedMagnitude:0.00000}");
+        var combinedDirectedMagnitudeThreshold = 0.0006f;
+        if (combinedDirectedMagnitude < combinedDirectedMagnitudeThreshold) {
+            return Vector2.zero;
+        }
+
+        // Debug.Log($"{dotProductLeft:0.00000} {dotProductRight:0.00000} {combined:0.00000}");
+
+        // var swingStrength = combined * (leftMagnitude + rightMagnitude);
+        // if (swingStrength >= 0.1 * PreviousPositionCount) {
+        IsDashing = true;
+        DashModifier = Mathf.Clamp(combinedDirectedMagnitude/combinedDirectedMagnitudeThreshold, 1, 2) - 0.5f;
+        // DashModifier = EaseDashModifier(combinedDirectedMagnitude, combinedDirectedMagnitudeThreshold, combinedDirectedMagnitudeThreshold*2, 0.5f, 2);
+        DashTime = 0;
+
+        Debug.LogWarning("DASH!");
+        ResetPositions();
+        // }
+
+        return GetCurrentDashVelocity(); 
+    }
+
+    private Vector2 GetCurrentDashVelocity() {
+        var magnitude = EaseOutExpo(DashTime / Duration);
+        return new Vector2(0, -magnitude); // This weirdness is left as an exercise for the reader
+    }
+
+    private float EaseOutExpo(float x) {
+        return 1 - Mathf.Pow(CurveExponent, -10 * (x-1));
+    }
+
+    private float EaseDashModifier(float swing, float minTarget, float maxTarget, float minMod, float maxMod) {
+        // https://www.desmos.com/calculator/004ai7no3p
+        float t = (swing - minTarget) / maxTarget;
+        t = Mathf.Clamp01(t);
+        return 1.5f - Mathf.Pow(1.5f - minMod - (maxMod - minMod) * t, 2);
     }
 }
