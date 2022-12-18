c94b79c5336b1958a458ca7a94583df4a9dbf84a
Martin <e11909911@student.tuwien.ac.at>
2022-12-18 06:12:57 +0100

    add fix

--- Assets/Resources/Scripts/Synchronization/SynchronizedMoveableSocketInteractor.cs
+++ Assets/Resources/Scripts/Synchronization/SynchronizedMoveableSocketInteractor.cs
@@ -5,19 +5,18 @@ using UnityEngine.XR.Interaction.Toolkit;
 
 [RequireComponent(typeof(PhotonView))]
 [RequireComponent(typeof(XRBaseInteractable))]
-public class SynchronizedMoveableSocketInteractor : MonoBehaviourPun, IOnPhotonViewOwnerChange {
+public class SynchronizedMoveableSocketInteractor : MonoBehaviourPun, IOnPhotonViewControllerChange {
     protected XRBaseInteractable Interactable;
     protected XRSocketInteractor Socket;
 
     protected virtual void Awake() => EstablishReferences();
-    protected virtual void Reset() => EstablishReferences();
 
     private void EstablishReferences() {
         Interactable = GetComponent<XRBaseInteractable>();
         Socket = GetComponentInChildren<XRSocketInteractor>();
     }
 
-    public void OnOwnerChange(Player newOwner, Player previousOwner) {
+    public void OnControllerChange(Player newOwner, Player previousOwner) {
         foreach (var interactable in Socket.interactablesSelected) {
             var photonView = PhotonView.Get(interactable.transform);
             photonView.RequestOwnership();
