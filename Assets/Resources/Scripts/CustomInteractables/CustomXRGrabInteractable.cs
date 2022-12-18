2360935e2eeefdbff11e7e35670b1d81fbffa935
Martin <e11909911@student.tuwien.ac.at>
2022-12-18 08:59:40 +0100

    Force gravity on detach

--- Assets/Resources/Scripts/CustomInteractables/CustomXRGrabInteractable.cs
+++ Assets/Resources/Scripts/CustomInteractables/CustomXRGrabInteractable.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.XR.Interaction.Toolkit;
 
 namespace Assets.Resources.Scripts {
@@ -15,6 +10,14 @@ namespace Assets.Resources.Scripts {
         protected override void SetupRigidbodyGrab(Rigidbody rigidbody) {
             rigidbody.isKinematic = WasKinematicValue;
             base.SetupRigidbodyGrab(rigidbody);
+        }
+
+        protected override void SetupRigidbodyDrop(Rigidbody rigidbody) {
+            base.SetupRigidbodyDrop(rigidbody);
+            rigidbody.isKinematic = WasKinematicValue;
+            if (forceGravityOnDetach) {
+                rigidbody.useGravity = true;
+            }
         }
 
     }
