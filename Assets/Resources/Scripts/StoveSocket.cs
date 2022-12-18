5d144ddd2b474e660f4956de847b34248c1221a9
Martin <e11909911@student.tuwien.ac.at>
2022-12-18 20:56:03 +0100

    Add stove fire, again

--- Assets/Resources/Scripts/StoveSocket.cs
+++ Assets/Resources/Scripts/StoveSocket.cs
@@ -1,7 +1,31 @@
+using UnityEngine;
 using UnityEngine.XR.Interaction.Toolkit;
 
 public class StoveSocket : XRSocketInteractor {
+    public ParticleSystem[] Fire;
+    // private PanSocket Pan;
+
     private bool IsPan(IXRInteractable interactable) => interactable.transform.gameObject.CompareTag(Tags.Pan);
     public override bool CanHover(IXRHoverInteractable interactable) => base.CanHover(interactable) && IsPan(interactable);
     public override bool CanSelect(IXRSelectInteractable interactable) => base.CanSelect(interactable) && IsPan(interactable);
+
+    protected override void OnSelectEntered(SelectEnterEventArgs args) {
+        base.OnSelectEntered(args);
+
+        // Pan = args.interactableObject.transform.gameObject.GetComponentInChildren<PanSocket>();
+
+        foreach (var system in Fire) {
+            system.Play();
+        }
+    }
+
+    protected override void OnSelectExited(SelectExitEventArgs args) {
+        base.OnSelectExited(args);
+
+        // Pan = args.interactableObject.transform.gameObject.GetComponentInChildren<PanSocket>();
+
+        foreach (var system in Fire) {
+            system.Stop();
+        }
+    }
 }
