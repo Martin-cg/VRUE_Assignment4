5859631eadc0673ab41df79d79ec47950ef07e59
Martin <e11909911@student.tuwien.ac.at>
2022-12-18 03:54:47 +0100

    Rewrite TransformParentSync

--- Assets/Resources/Scripts/Synchronization/TransformParentSync.cs
+++ Assets/Resources/Scripts/Synchronization/TransformParentSync.cs
@@ -2,39 +2,27 @@
 using UnityEngine;
 
 [RequireComponent(typeof(PhotonView))]
-public class TransformParentSync : SynchronizedRoomObject {
-    private SynchronizedRoomProperty<string[]> ScenePath;
+public class TransformParentSync : MonoBehaviourPun, IPunObservable {
+    private Transform LastParent;
+    private string[] LastPath;
 
-    protected override void Awake() {
-        base.Awake();
-
-        var key = $"{nameof(TransformParentSync)}-{photonView.ViewID}";
-        ScenePath = RegisterProperty<string[]>("ScenePath", null, key);
-        ScenePath.ValueChanged += (s, e) => OnScenePathPropertyChanged(e);
-    }
-
-    protected override void Start() {
-        base.Start();
-
-        UpdateScenePathProperty();
-    }
-
-    protected virtual void OnScenePathPropertyChanged(string[] scenePath) {
+    protected virtual void OnRemoteScenePathChanged(string[] scenePath) {
         transform.parent = Utils.ResolveScenePath(scenePath);
         Debug.Assert((scenePath.Length == 0) == (transform.parent == null));
     }
 
-    protected virtual void OnTransformParentChanged() {
-        UpdateScenePathProperty();
-    }
-
-    private void UpdateScenePathProperty() {
-        if (!PhotonNetwork.InRoom || !photonView.IsMine) {
-            return;
+    public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info) {
+        if (stream.IsReading) {
+            if (LastParent != transform.parent) {
+                LastParent = transform.parent;
+                LastPath = gameObject.GetScenePath(null, false).ToArray();
+            }
+            stream.SendNext(LastPath);
+        } else {
+            var newPath = stream.ReceiveNext<string[]>();
+            OnRemoteScenePathChanged(newPath);
+            LastPath = newPath;
+            LastParent = transform.parent;
         }
-
-        var newPath = gameObject.GetScenePath(null, false).ToArray();
-        Debug.LogWarning(string.Join("/", newPath));
-        ScenePath.SetValue(newPath, notifyLocal: true, notifyRemote: true);
     }
 }
